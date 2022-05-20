171a1472fd959676a34c0cf4f9090cb772ab2b3a
Mike Wright <m.wright92@icloud.com>
2022-05-20 09:29:46 -0700

    add project_id to Volunteer, pass spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,8 +1,9 @@
 class Volunteer
-  attr_reader :id, :name
+  attr_reader :id, :name, :project_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
   end
 end
