5f1c589aab36252ff1b8fb5aaadb0503ca89a608
Mike Wright <m.wright92@icloud.com>
2022-05-20 15:45:59 -0700

    update routes to reflect required integration specs

--- app.rb
+++ app.rb
@@ -18,10 +18,6 @@ get '/projects' do
   erb :projects
 end
 
-get '/projects/new' do
-  erb :new_project
-end
-
 #the .split .map added to title capitalizes the first letter in each word of the title
 post '/projects' do
   title = params[:project_title]
@@ -38,25 +34,4 @@ get '/projects/:id' do
   else
     erb :no_project
   end
-end
-
-get '/volunteers' do
-  @volunteers = Volunteer.all
-  erb :volunteers
-end
-
-get '/volunteers/new' do
-  erb :new_volunteer
-end
-
-post '/volunteers' do
-  name = params[:volunteer_name]
-  volunteer = Volunteer.new({:name => name, :project_id => nil, :id => nil})
-  volunteer.save
-  redirect to '/volunteers'
-end
-
-get '/volunteers/:id' do
-  @volunteer = Volunteer.find(params[:id].to_i)
-  erb :volunteer
 end
