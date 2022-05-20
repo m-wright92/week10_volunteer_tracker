fbaa1a8f40d330b917eed023b230a114c4963184
Mike Wright <m.wright92@icloud.com>
2022-05-20 10:14:43 -0700

    build save method, pass spec

--- lib/project.rb
+++ lib/project.rb
@@ -21,4 +21,9 @@ class Project
     end
     projects
   end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 end
